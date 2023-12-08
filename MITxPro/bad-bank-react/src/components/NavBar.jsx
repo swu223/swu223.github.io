cfbfc990ef2819dee9694a67c2b87fd153ed9550
swu223 <sarahwu223@gmail.com>
2023-12-08 12:21:13 -0500

    added home, create account, login nav items

--- MITxPro/bad-bank-react/src/components/NavBar.jsx
+++ MITxPro/bad-bank-react/src/components/NavBar.jsx
@@ -3,11 +3,17 @@ import { Navbar, Container, Nav } from 'react-bootstrap';
 
 export default function NavBar() {
   return(
-    <Navbar>
+    <Navbar expand="lg" className="bg-body-tertiary">
       <Container>
-        <Nav>
+        <Nav className="mr-auto">
           <Nav.Item>
             Home
+          </Nav.Item>
+          <Nav.Item>
+            Create Account
+          </Nav.Item>
+          <Nav.Item>
+            Login
           </Nav.Item>
         </Nav>
       </Container>
