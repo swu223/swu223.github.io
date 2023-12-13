76a400730ecf25390e770199549ce029b7fca573
swu223 <sarahwu223@gmail.com>
2023-12-13 09:26:19 -0500

    edited the onClick function to toggle between signed in and not signed in

--- MITxPro/bad-bank-react/src/containers/Home.jsx
+++ MITxPro/bad-bank-react/src/containers/Home.jsx
@@ -1,13 +1,20 @@
 import React, {useState, useEffect} from 'react';
 import NavBar from '../components/NavBar';
+import {Button} from 'react-bootstrap';
 
 
 export default function Home() {
+  const [signedIn, setSignedIn] = useState(false);
+
+  const handleClick = () =>{
+    setSignedIn(!signedIn);
+  }
+
   return(
     <div>
-      <NavBar/>
+      <NavBar signedIn={!signedIn}/>
       <h1>Welcome to the Bad Bank</h1>
-      
+      <Button onClick={handleClick}>Login</Button>
     </div>
   )
 };
