4e44f73eea0e631c3fc7c798b39e2ef787574a26
swu223 <sarahwu223@gmail.com>
2023-12-13 13:37:19 -0500

    changed create account page

--- MITxPro/bad-bank-react/src/components/CreateAccountForm.jsx
+++ MITxPro/bad-bank-react/src/components/CreateAccountForm.jsx
@@ -1,12 +1,38 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState } from 'react';
 import { BankContext } from '../contexts/BankContext';
+import { Card } from 'react-bootstrap';
 
 export default function CreateAccountForm() {
   const {data} = useContext(BankContext);
+  const [show, setShow]           = useState(true);
+  const [status, setStatus]       = useState('');
+  const [name, setName]           = useState('');
+  const [email, setEmail]         = useState('');
+  const [password, setPassword]   = useState('');
+
+  const handleCreate = () =>{
+    console.log(name, email, password);
+  }
 
   return (
-    <div> 
-      <h1>Create a new account</h1>
-    </div>
+    <Card
+      bgcolor="primary"
+      header="Create Account"
+      body={show? (
+        <>
+          Name<br/>
+          <input type="input" className="form-control" id="name" placeholder="Enter name" value={name} onChange={e => setName(e.currentTarget.value)} /><br/>
+          Email address<br/>
+          <input type="input" className="form-control" id="email" placeholder="Enter email" value={email} onChange={e => setEmail(e.currentTarget.value)}/><br/>
+          Password<br/>
+          <input type="password" className="form-control" id="password" placeholder="Enter password" value={password} onChange={e => setPassword(e.currentTarget.value)}/><br/>
+          <button type="submit" className="btn btn-light" onClick={handleCreate}>Create Account</button>
+        </>
+      ):(
+        <>
+        Success
+        </>
+      )}
+    />
   )
 };
--- MITxPro/bad-bank-react/src/containers/CreateAccount.jsx
+++ MITxPro/bad-bank-react/src/containers/CreateAccount.jsx
@@ -1,10 +1,39 @@
-import React from 'react';
-import CreateAccountForm from '../components/CreateAccountForm';
+import React, { useState, useContext } from 'react';
+// import CreateAccountForm from '../components/CreateAccountForm';
+import { Card, Button } from 'react-bootstrap';
+import { BankContext } from '../contexts/BankContext';
 
 export default function CreateAccount() {
+  const {data} = useContext(BankContext);
+  const [show, setShow]           = useState(true);
+  const [status, setStatus]       = useState('');
+  const [name, setName]           = useState('');
+  const [email, setEmail]         = useState('');
+  const [password, setPassword]   = useState('');
+
+  const handleCreate = () =>{
+    console.log(name, email, password);
+    
+  }
+
   return (
-    <div> Create Account Page 
-      <CreateAccountForm/>
-    </div>
+    <Card>
+      {show && (
+      <Card.Body>
+        <Card.Title>Create an account</Card.Title>
+          Name<br/>
+          <input type="input" className="form-control" id="name" placeholder="Enter name" value={name} onChange={e => setName(e.currentTarget.value)} /><br/>
+          Email address<br/>
+          <input type="input" className="form-control" id="email" placeholder="Enter email" value={email} onChange={e => setEmail(e.currentTarget.value)}/><br/>
+          Password<br/>
+          <input type="password" className="form-control" id="password" placeholder="Enter password" value={password} onChange={e => setPassword(e.currentTarget.value)}/><br/>
+          <Button type="submit" className="btn btn-light" onClick={handleCreate}>Create Account</Button>
+      </Card.Body>)}
+      {!show && (
+      <Card.Body>
+        <Card.Title>Success!</Card.Title>
+      </Card.Body>
+      )}
+    </Card>
   )
 };
