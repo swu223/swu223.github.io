200f4d78d9ad55e9fe34448259e7e279b44ea067
swu223 <sarahwu223@gmail.com>
2023-12-13 09:54:24 -0500

    added a router using routerprovider and createBrowserRouter

--- MITxPro/bad-bank-react/src/containers/Home.jsx
+++ MITxPro/bad-bank-react/src/containers/Home.jsx
@@ -1,6 +1,7 @@
 import React, {useState, useEffect} from 'react';
 import NavBar from '../components/NavBar';
 import {Button} from 'react-bootstrap';
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
 
 
 export default function Home() {
@@ -10,8 +11,19 @@ export default function Home() {
     setSignedIn(!signedIn);
   }
 
+  const appRouter = createBrowserRouter([
+    {
+      path:"/",
+      element:
+        <>
+          <NavBar/>
+        </>
+    }
+  ])
+
   return(
     <div>
+      <RouterProvider router={appRouter}/>
       <NavBar signedIn={!signedIn}/>
       <h1>Welcome to the Bad Bank</h1>
       <Button onClick={handleClick}>Login</Button>
