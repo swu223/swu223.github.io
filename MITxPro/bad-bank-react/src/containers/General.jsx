af20c6216f08929c6a0f70494385d87b138b89f7
swu223 <sarahwu223@gmail.com>
2023-12-13 09:53:55 -0500

    initial commit

---
+++ MITxPro/bad-bank-react/src/containers/General.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function General() {
+  return (
+    <div> 
+      General page to include a login section and some descriptions of products or what makes our bank great
+    </div>
+  )
+};
