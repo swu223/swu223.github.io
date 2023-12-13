87587b696d4c5ec41bb7f7e7ff8d59f7d7643651
swu223 <sarahwu223@gmail.com>
2023-12-13 09:24:22 -0500

    initial commit

---
+++ MITxPro/bad-bank-react/src/containers/AccountOverview.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function AccountOverview() {
+  return (
+    <div> Account Overview page to include the current balance and a history as well as what happens after you log in.
+
+    </div>
+  )
+};
