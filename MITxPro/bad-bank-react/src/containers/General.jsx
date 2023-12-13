b22502cd818fcdcb2db7c3c8b550b329fcbf0bed
swu223 <sarahwu223@gmail.com>
2023-12-13 10:12:23 -0500

    moved Welcome Title to the general page

--- MITxPro/bad-bank-react/src/containers/General.jsx
+++ MITxPro/bad-bank-react/src/containers/General.jsx
@@ -3,6 +3,7 @@ import React from 'react';
 export default function General() {
   return (
     <div> 
+      <h1>Welcome to the Bad Bank</h1>
       General page to include a login section and some descriptions of products or what makes our bank great
     </div>
   )
