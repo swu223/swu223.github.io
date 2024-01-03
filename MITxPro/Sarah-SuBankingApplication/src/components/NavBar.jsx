e6ab04f964868e2fb95d7a3b482247f0e607d237
swu223 <sarahwu223@gmail.com>
2024-01-03 17:46:13 -0500

    added AllData and edited the styling of the NavBar

--- MITxPro/Sarah-SuBankingApplication/src/components/NavBar.jsx
+++ MITxPro/Sarah-SuBankingApplication/src/components/NavBar.jsx
@@ -13,31 +13,23 @@ export default function NavBar() {
   return(
     <Navbar expand="lg" className="bg-body-tertiary">
       <Container>
-        <Navbar.Brand as={Link} to="/" href="#home">BadBank</Navbar.Brand>
+        <Navbar.Brand as={Link} to="/" href="#home" defaultActiveKey="/">BadBank</Navbar.Brand>
         <Navbar.Toggle/>
         <Navbar.Collapse id="responsive-navbar-nav">
-          {!signedIn && <Nav className="mr-auto">
-            <Nav.Link as={Link} to="/create-account">
-              Create Account
-            </Nav.Link>
-            <Nav.Link as={Link} to="/login">
-              Login
-            </Nav.Link>
-          </Nav>}
-          {signedIn && <Nav className="mr-auto">
-            <Nav.Link as={Link} to="/account-overview">
-              Account Overview
-            </Nav.Link>
-            <Nav.Link as={Link} to="/deposit">
-              Deposit
-            </Nav.Link>
-            <Nav.Link as={Link} to="/withdraw">
-              Withdraw
-            </Nav.Link>
-            <Nav.Link onClick={logOut} as={Link} to="/">
-              Signout
-            </Nav.Link>
-          </Nav>}
+          {!signedIn && 
+            <Nav className="mr-auto" variant="underline">
+              <Nav.Link as={Link} to="/create-account" eventKey="create-account">Create Account</Nav.Link>
+              <Nav.Link as={Link} to="/login" eventKey="login">Login</Nav.Link>
+              <Nav.Link as={Link} to="/alldata" eventKey="all-data">All Data</Nav.Link>
+            </Nav>}
+          {signedIn && 
+            <Nav className="mr-auto" variant="underline" defaultActiveKey="/account-overview">
+              <Nav.Link as={Link} to="/account-overview" eventKey="account-overview">Account Overview</Nav.Link>
+              <Nav.Link as={Link} to="/deposit" eventKey="deposit">Deposit</Nav.Link>
+              <Nav.Link as={Link} to="/withdraw" eventKey="withdraw">Withdraw</Nav.Link>
+              <Nav.Link as={Link} to="/alldata" eventKey="all-data">All Data</Nav.Link>
+              <Nav.Link onClick={logOut} as={Link} to="/">Signout</Nav.Link>
+            </Nav>}
         </Navbar.Collapse>
       </Container>
     </Navbar>
