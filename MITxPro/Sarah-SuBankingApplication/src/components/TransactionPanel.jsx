b9aa670b631e9c20a9f772a1ff88dd83c2981356
swu223 <sarahwu223@gmail.com>
2024-01-06 10:29:11 -0500

    destructured userData and put into use. Added props to Transaction Panel accordingly

--- MITxPro/Sarah-SuBankingApplication/src/components/TransactionPanel.jsx
+++ MITxPro/Sarah-SuBankingApplication/src/components/TransactionPanel.jsx
@@ -3,10 +3,12 @@ import {Button, Card} from 'react-bootstrap';
 import { useNavigate } from 'react-router-dom';
 import { BankContext } from '../contexts/BankContext';
 
-export default function TransactionPanel() {
+export default function TransactionPanel({
+  current_balance
+}) {
   return (
     <Card>
-      <h1>Current Balance:</h1>
+      <h1>Current Balance: ${current_balance}</h1>
       Deposit
       <input type="number"></input>
       <input type ="submit"></input>
--- MITxPro/Sarah-SuBankingApplication/src/containers/Deposit.jsx
+++ MITxPro/Sarah-SuBankingApplication/src/containers/Deposit.jsx
@@ -1,11 +1,21 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import {Card} from 'react-bootstrap';
 import TransactionPanel from '../components/TransactionPanel';
+import { BankContext } from '../contexts/BankContext'
 
 export default function Deposit() {
+  const {data, userID}  = useContext(BankContext);
+  const userData = data.find((acc)=> acc.account_id === userID);
+  console.log(userData)
+  const {account_id, balance:{current_balance, transactions}, user} = userData;
+
+
+
+
   return (
     <Card>
-    <TransactionPanel></TransactionPanel>
+    <TransactionPanel current_balance={current_balance}>
+    </TransactionPanel>
     </Card>
   )
 };
