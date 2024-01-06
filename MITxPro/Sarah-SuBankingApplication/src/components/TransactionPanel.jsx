715e85320494e7cfe5a09ca2a3392b3f707a3053
swu223 <sarahwu223@gmail.com>
2024-01-06 09:57:21 -0500

    initial commit

---
+++ MITxPro/Sarah-SuBankingApplication/src/components/TransactionPanel.jsx
@@ -0,0 +1,15 @@
+import React, { useState, useContext } from 'react';
+import {Button, Card} from 'react-bootstrap';
+import { useNavigate } from 'react-router-dom';
+import { BankContext } from '../contexts/BankContext';
+
+export default function TransactionPanel() {
+  return (
+    <Card>
+      Current Balance:
+      Deposit
+      <input type="number"></input>
+      <input type ="submit"></input>
+    </Card>
+  )
+};
