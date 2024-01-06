bb754a3ac4226e6ecd3f6dbedd445cbf40fc5f71
swu223 <sarahwu223@gmail.com>
2024-01-06 09:41:21 -0500

    added account overview and history of transactions

--- MITxPro/Sarah-SuBankingApplication/src/containers/AccountOverview.jsx
+++ MITxPro/Sarah-SuBankingApplication/src/containers/AccountOverview.jsx
@@ -1,9 +1,25 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import {Button, Card} from 'react-bootstrap';
+import { useNavigate } from 'react-router-dom';
+import { BankContext } from '../contexts/BankContext';
 
 export default function AccountOverview() {
-  return (
-    <div> Account Overview page to include the current balance and a history as well as what happens after you log in.
+  const {data, userID} = useContext(BankContext);
 
-    </div>
+  const userData = data.find((acc)=> acc.account_id === userID);
+  console.log(userData)
+  console.log("current balance", userData.balance.current_balance)
+
+  return (
+    <>
+    <Card>
+      <h1>Account Overview</h1>
+      <h2>Current Balance: </h2>
+      <h2>{userData.balance.current_balance}</h2>
+    </Card>
+    <Card>
+      <h1>History of transactions</h1>
+    </Card>
+    </>
   )
 };
