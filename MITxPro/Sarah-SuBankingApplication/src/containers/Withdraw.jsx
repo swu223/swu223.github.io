df9a8cd60ddd6495dd01ee81218642bb8a5c159e
swu223 <sarahwu223@gmail.com>
2024-01-06 10:29:38 -0500

    added imports to Withrdaw, copying Deposit

--- MITxPro/Sarah-SuBankingApplication/src/containers/Withdraw.jsx
+++ MITxPro/Sarah-SuBankingApplication/src/containers/Withdraw.jsx
@@ -1,9 +1,10 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import {Card} from 'react-bootstrap';
 import TransactionPanel from '../components/TransactionPanel';
-
+import { BankContext } from '../contexts/BankContext'
 
 export default function Withdraw() {
+  const {data, userID}  = useContext(BankContext);
   return (
     <Card>
     <TransactionPanel>
