4bad094a58b31e53b18896e4dfe48e0bda1b0523
swu223 <sarahwu223@gmail.com>
2024-01-06 10:54:10 -0500

    added transactionType

--- MITxPro/Sarah-SuBankingApplication/src/containers/Withdraw.jsx
+++ MITxPro/Sarah-SuBankingApplication/src/containers/Withdraw.jsx
@@ -5,6 +5,10 @@ import { BankContext } from '../contexts/BankContext'
 
 export default function Withdraw() {
   const {data, userID}  = useContext(BankContext);
+  const userData = data.find((acc)=> acc.account_id === userID);
+  const {account_id, balance:{current_balance, transactions}, user} = userData;
+
+  const transactionType = "Withdrawal";
   return (
     <Card>
     <TransactionPanel>
