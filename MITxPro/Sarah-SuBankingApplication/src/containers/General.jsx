08710c0ba9c7987136b4e771b4931b37c4f3b20d
swu223 <sarahwu223@gmail.com>
2024-01-06 13:00:45 -0500

    edited welcome message

--- MITxPro/Sarah-SuBankingApplication/src/containers/General.jsx
+++ MITxPro/Sarah-SuBankingApplication/src/containers/General.jsx
@@ -3,14 +3,16 @@ import LoginPanel from '../components/LoginPanel';
 import { BankContext } from '../contexts/BankContext';
 
 export default function General() {
-  const {data} = useContext(BankContext);
 
   return (
     <div> 
       <h1>Welcome to the Bad Bank</h1>
-      General page to include a login section and some descriptions of products or what makes our bank great
-      <h3>This is why you should use this Bank blah blah blah</h3>
+      So bad that it's good.
+      <h3>Our Bank offers a lot of wonderful features!</h3>
       <LoginPanel/>
+      <div>Don't have an account? 
+        Create one here.
+      </div>
     </div>
   )
 };
