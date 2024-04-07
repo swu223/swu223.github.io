214747920a3ca7f5334975121e18ddf94c1e24a8
swu223 <sarahwu223@gmail.com>
2024-04-07 16:46:30 -0400

    trying to connect to db

--- MITxPro/bank-app-api/server.mjs
+++ MITxPro/bank-app-api/server.mjs
@@ -2,13 +2,17 @@ import express from 'express';
 import bodyParser from 'body-parser';
 const app = express();
 const PORT = 3000;
-
+import db from './db.mjs';
 
 app.use(bodyParser.json());
 
 app.get('/', (req,res)=>{
   res.send('Hello serverfolks');
 })
+
+app.post('/test'), async (req,res) => {
+  
+}
 
 app.listen(PORT, ()=>{
   console.log(`bank app server running on PORT:${PORT}`)
