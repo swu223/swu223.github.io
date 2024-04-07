47e5742b9e727de266fb6afb3245153dba9e75c2
swu223 <sarahwu223@gmail.com>
2024-04-07 16:41:26 -0400

    added diconnect to db.mjs

--- MITxPro/bank-app-api/db.mjs
+++ MITxPro/bank-app-api/db.mjs
@@ -1,11 +1,24 @@
 import mongoose from 'mongoose';
 
 const connect = () =>{
-
+  const url = porocess.env.MONGO_CONNECTION_STRING;
+  mongoose.connect(url);
+  mongoose.connection.once("open", async () => {
+    console.log("Connected to database");
+  });
+  mongoose.connection.on("error", (err) => {
+    console.error("Error connection to database  ", err);
+  }
 }
 
 const disconnect = () => {
-
-}
+  if (!mongoose.connection) {
+    return;
+  }
+  mongoose.disconnect();
+  mongoose.connection.once("close", async () => {
+      console.log("Diconnected  to database");
+  });
+};
 
 export default {connect, disconnect}
