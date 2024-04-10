6fd3e37be3745cb420d8252c356b8449e86755ba
swu223 <sarahwu223@gmail.com>
2024-04-10 11:42:42 -0400

    added connections, post-signup and post-test

--- MITxPro/bank-app-api/server.mjs
+++ MITxPro/bank-app-api/server.mjs
@@ -1,8 +1,19 @@
 import express from 'express';
 import bodyParser from 'body-parser';
+import dotenv from 'dotenv';
 const app = express();
 const PORT = 3000;
 import db from './db.mjs';
+import { UserController } from './entities/user.controller.mjs';
+
+// Configure dotenv
+dotenv.config();
+
+//connect to MongoDB container
+db.connect();
+
+// initialize userController
+const userControl = new UserController();
 
 app.use(bodyParser.json());
 
@@ -10,9 +21,14 @@ app.get('/', (req,res)=>{
   res.send('Hello serverfolks');
 })
 
-app.post('/test'), async (req,res) => {
-  
-}
+app.post('/signup', async (req, res)=>{
+  const response = await userControl.signup(req.body.user);
+  res.send(response)
+})
+
+app.post('/test', async (req,res) => {
+  res.send('This is hard')
+})
 
 app.listen(PORT, ()=>{
   console.log(`bank app server running on PORT:${PORT}`)
