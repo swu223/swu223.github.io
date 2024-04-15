d50cad622cdc75e2a4956898f3477eaf0e6ec9d4
swu223 <sarahwu223@gmail.com>
2024-04-14 22:43:51 -0400

    makeing handleLoginDB manage if there is already a token... jk probably not

--- MITxPro/Sarah-SuBankingApplication/src/utils/endpoints/auth.js
+++ MITxPro/Sarah-SuBankingApplication/src/utils/endpoints/auth.js
@@ -2,22 +2,58 @@ import axios from 'axios';
 
 const handleSignupDB = async (acc) => {
   try {
-    console.log(acc.user.name, "was created via handleSignupDB")
     //get the info from database
+    const response = await axios.post("http://localhost:3001/signup", {
+      acc
+    })
     // if created, receive a token
+    if (response.data.token) {
+      console.log("received token in the front end: ", response.data.token)
+      localStorage.setItem("token", response.data.token)
+      console.log(acc.name, "was created via handleSignupDB")
+      return true
+    }
     // if not, return false
+    return false
   } catch (err) {
     console.error(err);
     return false
   }
 }
 
-const handleLoginDB = async (acc) => {
+const handleLoginDB = async (loginInfo) => {
   try {
-    console.log(acc)
-    //send login info to database
-    //if correct, receive a token
-    // if not, return false
+    if (localStorage.getItem("token")) {
+      // if you just created acct, then make config wtih token and send to db and check if it matches
+      const config = {
+        headers:{
+          Authorization: `Bearer ${localStorage.getItem('token')}`
+        }
+      }
+      const response = await axios.post(
+        "http://localhost:3001/checktoken", 
+        config, 
+        {loginInfo}
+      )
+      if (response) {
+        return true
+      }
+      return false;
+    } else {
+      //send login info to database
+      const response = await axios.post("http://localhost:3001/login", {
+        loginInfo
+      })
+      //if correct, receive a token
+      if (response.data.token) {
+        localStorage.setItem("token", response.data.token)
+        return true
+      } 
+      // if not, return false
+      return false 
+    }
+    
+  
   } catch (err) {
     console.error(err);
     return false
@@ -28,4 +64,14 @@ const handleLogoutDB = async (acc) => {
   // remove token
 }
 
-export {handleSignupDB, handleLogoutDB, handleLoginDB}
+const checkToken = async (token, userInfo) => {
+  console.log(token, userInfo)
+  try {
+
+  } catch (err) {
+    console.error(err)
+    return false
+  }
+}
+
+export {handleSignupDB, handleLogoutDB, handleLoginDB, checkToken}
