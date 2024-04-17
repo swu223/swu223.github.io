a4047da3f1a2207cee0649a400eef7cb9ac8c091
swu223 <sarahwu223@gmail.com>
2024-04-17 11:14:23 -0400

    changed create account button to link instead

--- MITxPro/Sarah-SuBankingApplication/src/containers/Login.jsx
+++ MITxPro/Sarah-SuBankingApplication/src/containers/Login.jsx
@@ -1,12 +1,15 @@
 import React from 'react';
-import {Card} from 'react-bootstrap';
+import {Card, Button} from 'react-bootstrap';
 import LoginPanel from '../components/LoginPanel';
+import { useNavigate } from 'react-router-dom';
 
 export default function Login() {
+  const navigate = useNavigate();
   return (
     <Card>
       <LoginPanel>        
       </LoginPanel>
+      <div>Don't have an account? <a href="/create-account">here</a>.</div>
     </Card>
   )
 };
