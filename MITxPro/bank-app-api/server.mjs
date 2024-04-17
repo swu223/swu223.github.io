5fc398d3cdecc87b614b41b9cdb734e09829fb2c
swu223 <sarahwu223@gmail.com>
2024-04-17 11:34:47 -0400

    attempted writing 'my data' endpoint

--- MITxPro/bank-app-api/server.mjs
+++ MITxPro/bank-app-api/server.mjs
@@ -1,18 +1,21 @@
 import express from 'express';
 import bodyParser from 'body-parser';
+import cors from 'cors';
 import dotenv from 'dotenv';
 const app = express();
 const PORT = 3001;
-import db from './db.mjs';
 import { UserController } from './entities/user.controller.mjs';
 import { authorizeRequest } from './middleware/jwtConfig.mjs';
 
 // Configure dotenv
 dotenv.config();
 
-
 // initialize userController
 const userControl = new UserController();
+
+app.use(cors({
+  origin: ['http://localhost:3000']
+}))
 
 app.use(bodyParser.json());
 
@@ -21,11 +24,18 @@ app.get('/', (req,res)=>{
 })
 
 app.post('/signup', async (req, res)=>{
-  const response = await userControl.signup(req.body.user);
+  console.log("server level user: ", req.body.acc)
+  const response = await userControl.signup(req.body.acc);
   res.send(JSON.stringify(response))
 })
 
+app.get('/mydata', authorizeRequest, (req,res) => {
+  console.log('my data will go here');
+  res.send('here is data from server')
+})
+
 app.post('/test', authorizeRequest, (req,res) => {
+  console.log(req.headers)
   res.send('This is hard')
 })
 
