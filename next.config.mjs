737f8a5e717a7038e96059866562bbce6d44d3df
swu223 <sarahwu223@gmail.com>
2024-10-01 14:21:57 -0400

    added next.config, public no jekyll file and a github workflow to setup node

---
+++ next.config.mjs
@@ -0,0 +1,25 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  /** 
+   * Enable static exports for the App Router
+   * 
+   * @see https://nextjs.org/docs/app/building-your-application/deploying/static-exports
+   */
+  output: "export",
+
+  /**
+   * Set base path. This is the slug of your GitHub repository
+   * 
+   * @see https://next.js.org/docs/app/api-reference/next-config-js/basePath
+   */
+  basePath: "/swu223.github.io"
+
+  /**
+   * Disable server-based image optimization 
+   * Next.js doesn't support dynamic features with statis exports
+   */
+
+
+};
+
+export default nextConfig;
