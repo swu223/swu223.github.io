f860957b6b0e1f414eba5150f0951f12dbe2b829
swu223 <sarahwu223@gmail.com>
2024-10-23 11:37:31 -0400

    added lorem ipsum text to the page. added navigation links to navbar to jump to project section of the page.

--- app/ui/navbar.tsx
+++ app/ui/navbar.tsx
@@ -1,7 +1,13 @@
+import Link from "next/link";
+
 export default function NavBar() {
   return (
-    <div>Navigation Bar goes here
+    <div>
+      Navigation Bar goes here.
+
+      <Link href="/">Home</Link>
       
+      <Link href="/#projects">Projects</Link>
     </div>
   )
 }
