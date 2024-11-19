f457c5d481d9b1434c76ed9c4d886218a515ba94
swu223 <sarahwu223@gmail.com>
2024-11-19 10:45:14 -0500

    added lorem ipsum to projects container

---
+++ app/ui/projects.tsx
@@ -0,0 +1,18 @@
+export default function Projects() {
+  return (
+    <div className="flex flex-col px-8">
+      <h1 id="projects">Projects</h1>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam eget mauris sit amet arcu scelerisque blandit eget id leo. Quisque vitae gravida nulla, non commodo risus. Nullam lobortis arcu ac massa sollicitudin vulputate. Proin id ipsum tempor, cursus erat tempus, pulvinar lacus. Vestibulum gravida nulla lacus. Vivamus eleifend augue luctus risus luctus, ut vehicula eros malesuada. Morbi a porta nunc, vitae tincidunt quam. Proin congue laoreet viverra. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae;
+
+        Pellentesque a erat erat. In ornare facilisis nulla. Praesent varius scelerisque felis nec malesuada. Donec malesuada dui vitae ante consectetur blandit. Sed scelerisque malesuada diam ac sagittis. Donec ultricies interdum commodo. Duis sed odio nec libero convallis eleifend id eget lorem. Nam pulvinar scelerisque felis, at placerat turpis imperdiet nec. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas facilisis dignissim sollicitudin. Aliquam et massa vel orci faucibus ultrices. Nulla fringilla sed felis eget iaculis. Phasellus pretium malesuada pharetra. Nam cursus mauris nec neque accumsan, non sagittis libero euismod.
+
+        Sed id metus quis nulla gravida laoreet vitae eget dui. Curabitur eget nunc orci. Aenean facilisis hendrerit nisl, eget condimentum diam venenatis nec. Nulla facilisi. Sed pretium enim nunc, eget sagittis orci accumsan ut. Quisque feugiat quam et nisi gravida faucibus a vel tellus. Sed vitae cursus mauris, sit amet varius arcu. Cras quis enim eu dolor facilisis aliquam mollis nec neque. Praesent convallis justo mauris, pulvinar placerat ipsum aliquet ac. Mauris sodales laoreet semper. Fusce sed orci odio. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Etiam egestas euismod convallis.
+
+        Pellentesque ultrices metus ac pretium eleifend. Ut et erat nisl. Vivamus malesuada erat sit amet ex aliquam volutpat. Integer quis mattis magna. Proin accumsan pharetra dolor quis tristique. In sed congue odio, et consectetur nibh. Proin tortor massa, imperdiet at libero eget, sagittis accumsan mauris. Cras a laoreet purus. Aenean suscipit commodo dolor. Phasellus eu quam nec leo aliquet vestibulum et ut nisi.
+
+        Integer tempus est eget leo luctus lobortis. Nam imperdiet ex id est condimentum tempus. Quisque vitae facilisis ipsum. Vestibulum vitae consequat libero. Suspendisse quis vestibulum est. Vestibulum et ornare nulla. Sed interdum nisi quis sem gravida, ac bibendum magna ultrices. Etiam id consectetur erat. Maecenas iaculis et felis eu porta. Donec lacinia eros et lectus finibus, quis imperdiet lectus consequat. Curabitur id turpis ex. Duis euismod felis quis efficitur malesuada. In blandit arcu mi, a aliquet mauris maximus eget. Maecenas elementum metus sed diam vehicula, ac facilisis nisl efficitur. Aenean viverra, purus quis blandit eleifend, purus urna semper libero, et
+      </p>
+    </div>
+  )
+}
