7257b6dc8f0f3160c47cfcdc963375cb808e4a72
swu223 <sarahwu223@gmail.com>
2024-11-19 10:36:40 -0500

    added to-do note to self regarding making the resume appear functionally

--- app/ui/about-me.tsx
+++ app/ui/about-me.tsx
@@ -17,6 +17,7 @@ export default function AboutMe() {
         <div id="resume" className="h-72 overflow-auto col-start-2 col-span-2 row-span-2">
           <h2> Resume </h2> 
           <p>
+            {/* To do: make the resume populate via a function and a text document so that the formatting updates accordingly...? */}
             rem ipsum dolor sit amet, consectetur adipiscing elit. Nam eget mauris sit amet arcu scelerisque blandit eget id leo. Quisque vitae gravida nulla, non commodo risus. Nullam lobortis arcu ac massa sollicitudin vulputate. Proin id ipsum tempor, cursus erat tempus, pulvinar lacus. Vestibulum gravida nulla lacus. Vivamus eleifend augue luctus risus luctus, ut vehicula eros malesuada. Morbi a porta nunc, vitae tincidunt quam. Proin congue laoreet viverra. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae;
 
             Pellentesque a erat erat. In ornare facilisis nulla. Praesent varius scelerisque felis nec malesuada. Donec malesuada dui vitae ante consectetur blandit. Sed scelerisque malesuada diam ac sagittis. Donec ultricies interdum commodo. Duis sed odio nec libero convallis eleifend id eget lorem. Nam pulvinar scelerisque felis, at placerat turpis imperdiet nec. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas facilisis dignissim sollicitudin. Aliquam et massa vel orci faucibus ultrices. Nulla fringilla sed felis eget iaculis. Phasellus pretium malesuada pharetra. Nam cursus mauris nec neque accumsan, non sagittis libero euismod.
