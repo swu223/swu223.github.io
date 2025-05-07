0028424074461981c3a55148bf18ba18446bdf5b
swu223 <sarahwu223@gmail.com>
2025-05-07 12:03:00 -0400

    removed projects 4-6, added EMR project, nextjs dashboard project and banking application basic description to the projects

--- app/ui/project-cards.tsx
+++ app/ui/project-cards.tsx
@@ -2,71 +2,43 @@ import Link from "next/link";
 
 const projectItems = [
   {
-    name: "project1",
-    date: "June 2024",
+    name: "Mock EMR app",
+    date: "January 2025-Present",
     languages: [
       "Javascript", 
-      "Node.js"
+      "Node.js",
+      "Express.js",
+      "PostgreSQL"
+    ],
+    demo_link: "",
+    github_link: "https://github.com/swu223/emr-mock-server",
+    description: "A mock Electric Medical Records app for doctors, office staff and patients"
+  },
+  {
+    name: "Nextjs dashboard",
+    date: "Oct 2024",
+    languages: [ 
+      "Node.js",
+      "Typscript",
+
+    ],
+    demo_link: "https://nextjs-dashboard-4tpa1pajk-sarah-w-sus-projects.vercel.app/",
+    github_link: "https://github.com/swu223/nextjs-dashboard",
+    description: "A basic dashboard utilizing the next.js framework and based on the tutorial from https://nextjs.org/learn."
+  },
+  {
+    name: "Banking App",
+    date: "May 2024",
+    languages: [
+      "Javascript", 
+      "Node.js",
+      "MongoDB",
+      "AWS",
     ],
     demo_link: "https://google.com",
     github_link: "https://github.com/swu223",
     description: "rem ipsum dolor sit amet, consectetur adipiscing elit. Nam eget mauris sit amet arcu scelerisque blandit eget id leo. Quisque vitae gravida nulla, non commodo risus. Nullam lobortis arcu ac massa sollicitudin vulputate. Proin id ipsum tempor, cursus erat tempus, pulvinar lacus. Vestibulum gravi"
-  },
-  {
-    name: "project2",
-    date: "Oct 2024",
-    languages: [
-      "Javascript", 
-      "Node.js"
-    ],
-    demo_link: "https://google.com",
-    github_link: "https://github.com/swu223",
-    description: "rem ipsum dolor sit amet, consectetur adipiscing elit. Nam eget mauris sit amet arcu scelerisque blandit eget id leo. Quisque vitae gravida nulla, non commodo risus. Nullam lobortis arcu ac massa sollicitudin vulputate. Proin id ipsum tempor, cursus erat tempus, pulvinar lacus. Vestibulum gravi"
-  },
-  {
-    name: "project3",
-    date: "Oct 2024",
-    languages: [
-      "Javascript", 
-      "Node.js"
-    ],
-    demo_link: "https://google.com",
-    github_link: "https://github.com/swu223",
-    description: "rem ipsum dolor sit amet, consectetur adipiscing elit. Nam eget mauris sit amet arcu scelerisque blandit eget id leo. Quisque vitae gravida nulla, non commodo risus. Nullam lobortis arcu ac massa sollicitudin vulputate. Proin id ipsum tempor, cursus erat tempus, pulvinar lacus. Vestibulum gravi"
-  },
-  {
-    name: "project4",
-    date: "Oct 2024",
-    languages: [
-      "Javascript", 
-      "Node.js"
-    ],
-    demo_link: "https://google.com",
-    github_link: "https://github.com/swu223",
-    description: "Pellentesque a erat erat. In ornare facilisis nulla. Praesent varius scelerisque felis nec malesuada. Donec malesuada dui vitae ante consectetur blandit. Sed scelerisque malesuada diam ac sagittis. Donec ultricies interdum commodo. Duis sed odio nec libero convallis eleifend id eget lorem. rem ipsum dolor sit amet, consectetur adipiscing elit. Nam eget mauris sit amet arcu scelerisque blandit eget id leo. Quisque vitae gravida nulla, non commodo risus. Nullam lobortis arcu ac massa sollicitudin vulputate. Proin id ipsum tempor, cursus erat tempus, pulvinar lacus. Vestibulum gravi"
-  },
-  {
-    name: "project5",
-    date: "Oct 2024",
-    languages: [
-      "Javascript", 
-      "Node.js"
-    ],
-    demo_link: "https://google.com",
-    github_link: "https://github.com/swu223",
-    description: "Pellentesque a erat erat. In ornare facilisis nulla. Praesent varius scelerisque felis nec malesuada. Donec malesuada dui vitae ante consectetur blandit. Sed scelerisque malesuada diam ac sagittis. Donec ultricies interdum commodo. Duis sed odio nec libero convallis eleifend id eget lorem. rem ipsum dolor sit amet, consectetur adipiscing elit. Nam eget mauris sit amet arcu scelerisque blandit eget id leo. Quisque vitae gravida nulla, non commodo risus. Nullam lobortis arcu ac massa sollicitudin vulputate. Proin id ipsum tempor, cursus erat tempus, pulvinar lacus. Vestibulum gravi"
-  },
-  {
-    name: "project6",
-    date: "Oct 2024",
-    languages: [
-      "Javascript", 
-      "Node.js"
-    ],
-    demo_link: "https://google.com",
-    github_link: "https://github.com/swu223",
-    description: "Pellentesque a erat erat. In ornare facilisis nulla. Praesent varius scelerisque felis nec malesuada. Donec malesuada dui vitae ante consectetur blandit. Sed scelerisque malesuada diam ac sagittis. Donec ultricies interdum commodo. Duis sed odio nec libero convallis eleifend id eget lorem. rem ipsum dolor sit amet, consectetur adipiscing elit. Nam eget mauris sit amet arcu scelerisque blandit eget id leo. Quisque vitae gravida nulla, non commodo risus. Nullam lobortis arcu ac massa sollicitudin vulputate. Proin id ipsum tempor, cursus erat tempus, pulvinar lacus. Vestibulum gravi"
-  },
+  }
 ]
 
 export default function ProjectCards() {
